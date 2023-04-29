65dd9f7b0558a8c39b18756e912a073bb9726198
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-04-29 12:36:21 -0400

    installed .dotenv and setup openai

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
+const {Configuration, OpenAIApi} = require('openai');
+
+app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello There From Nodejs Server')
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_API
+})
+
+const openai = new OpenAIApi(configuration)
+
+app.get('/', async (req, res) => {
+    const response = await openai.createCompletion({
+        prompt: 'Give Me a Funny Word',
+        model: 'text-davinci-003',
+    })
+    let data = response.data.choices[0].text;
+    
+    res.render('pages/index', {
+        data
+    })
 })
 
 app.listen(PORT, () => console.log(`App is running on PORT ${PORT}`))
