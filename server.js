04bce1c4b25b454bf834807f3d0763f9350c0242
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2023-05-01 13:01:46 -0400

    update api key and added palette endpoint

--- server.js
+++ server.js
@@ -14,15 +14,34 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 app.get('/', async (req, res) => {
+    res.render('pages/index')
+})
+
+app.post('/palette', async (req, res) => {
+    let msg = req.body.msg || 'colors of italian flag';
+    let prompt = `
+    You are a color palette generating assistant that responds to text prompts for color palettes
+    Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
+    The palettes should be between 2 and 8 colors.
+
+    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
+    A: ["#006699", "#66CCCC", "#F0E68C", "#008000", "#F08080"]
+
+    Q: Convert the following verbal description of a color palette into a list of colors: sage, nature, earth
+    A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
+
+
+    Desired Format: a JSON array of hexadecimal color codes
+
+    Q: Convert the following verbal description of a color palette into a list of colors: ${msg} 
+    A:
+`
     const response = await openai.createCompletion({
-        prompt: 'Give Me a Funny Word',
+        prompt: prompt,
         model: 'text-davinci-003',
+        max_tokens:200,
     })
     let data = response.data.choices[0].text;
-    
-    res.render('pages/index', {
-        data
-    })
 })
 
 app.listen(PORT, () => console.log(`App is running on PORT ${PORT}`))
